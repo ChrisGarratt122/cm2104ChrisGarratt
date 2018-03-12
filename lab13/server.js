cf1e8a6e1f3d764f4b16e99d7440cc59f541a5b8
CHRIS GARRATT (1704899) <1704899@rgu.ac.uk>
2018-03-12 16:03:47 +0000

    Lab 13 Quick Task, adding route joke

--- lab13/server.js
+++ lab13/server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var oneLinerJoke = require('one-liner-joke');
 app.get('/', function(req, res){
  res.send("Hello world! by express");
 });
 app.get('/test', function(req, res){
  res.send("this is route 2");
+});
+app.get('/joke', function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+   var randomJoke = oneLinerJoke.getRandomJoke();
+   res.end(randomJoke.body);
 });
 app.listen(8080);
