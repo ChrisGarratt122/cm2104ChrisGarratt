1d2b8ecd1665e672c44e228588722444cbcfd712
CHRIS GARRATT (1704899) <1704899@rgu.ac.uk>
2018-03-12 16:06:58 +0000

    Lab 13 Adding Route for Accessing Parameters

--- lab13/server.js
+++ lab13/server.js
@@ -12,4 +12,9 @@ app.get('/joke', function(req, res){
    var randomJoke = oneLinerJoke.getRandomJoke();
    res.end(randomJoke.body);
 });
+app.get('/add', function(req, res){
+var x = req.query.x;
+var y = req.query.y;
+ res.send("X + Y="+(x+y));
+});
 app.listen(8080);
